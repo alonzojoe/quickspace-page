161e1be1286626de982fc2261d4790c4d593a112
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-30 09:26:17 +0800

    create constants for HERO_CONTENT and BRAND_ITEMS use app image assets for hero and brands

---
+++ src/constants/index.ts
@@ -0,0 +1,23 @@
+
+
+
+import Hero from "../assets/images/hero.png"
+import Brand1 from "../assets/images/brand1.png"
+import Brand2 from "../assets/images/brand2.png"
+import Brand3 from "../assets/images/brand3.png"
+import Brand4 from "../assets/images/brand4.png"
+
+export const HERO_CONTENT = {
+    title: 'Smart-Thinking & Innovative Solution.',
+    subTitlte: 'Class aptent taciti sociosqu ad litora torquent conubia nostrama inceptos himenaeos. Donec tristique nibh ipsum dignissim.',
+    button1: 'Discover More',
+    button2: 'Explore Service',
+    items: [
+        'Get 15 days free trial',
+        'No credit card is required',
+        'Cancel anytime'
+    ],
+    heroImg: Hero
+}
+
+export const BRAND_ITEMS = [Brand1, Brand2, Brand3, Brand4]
