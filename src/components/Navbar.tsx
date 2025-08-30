4d5d98b90c7bfcc25d37adaf8e7d7e553e434de9
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-30 23:44:04 +0800

    Navbar components add responsiveness create state for toggle and style menu mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import Logo from "../assets/images/logo.png";
-import { TfiMenu } from "react-icons/tfi";
+import { TbMenuDeep, TbX } from "react-icons/tb";
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const toggleMenu = () => {
+    setToggle((prev) => !prev);
+  };
+
+  const Icon = toggle ? TbX : TbMenuDeep;
+
   return (
     <nav className="fixed top-4 right-0 left-0 z-100">
       <div className="bg-white/60 backdrop-blur-md max-w-7xl mx-auto px-4 py-2 flex items-center justify-between">
@@ -23,12 +32,44 @@ const Navbar = () => {
             <a href="#contact">Contact</a>
           </li>
         </ul>
-        <button className="hidden md:block bg-primary text-white text-lg py-2 px-4 rounded-lg">
+        <button className="hidden md:block bg-primary cursor-pointer hover:bg-lightPrimary text-white text-lg py-2 px-4 rounded-lg">
           Get Started Free
         </button>
         <div className="md:hidden">
-          <TfiMenu className="text-dark cursor-pointer text-3xl" />
+          <Icon
+            onClick={toggleMenu}
+            className="text-dark cursor-pointer text-4xl"
+          />
         </div>
+      </div>
+
+      <div
+        className={`md:hidden p-4 bg-white/60 backdrop-blur-md border border-gray-300 rounded-xl mt-2 mx-2 transition-all ease-in-out duration-300 transform ${
+          toggle
+            ? "opacity-100 translate-y-0 scale-100"
+            : "opacity-0 -translate-y-2 scale-95 pointer-events-none"
+        }`}
+      >
+        <ul className="flex flex-col text-secondary font-semibold text-md space-y-4">
+          <li className="hover:text-dark">
+            <a href="#home">Home</a>
+          </li>
+          <li className="hover:text-dark">
+            <a href="#page">Page</a>
+          </li>
+          <li className="hover:text-dark">
+            <a href="#portfolio">Portfolio</a>
+          </li>
+          <li className="hover:text-dark">
+            <a href="#blog">Blog</a>
+          </li>
+          <li className="hover:text-dark">
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+        <button className="bg-primary hover:bg-lightPrimary cursor-pointer mt-4 text-white text-lg py-2 px-4 rounded-lg">
+          Get Started Free
+        </button>
       </div>
     </nav>
   );
