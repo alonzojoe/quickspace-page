7045e79ef2b4bc3d36165032cb9cb59ee4dd0774
alonzojoe <alonzojoenell2k15@gmail.com>
2025-09-02 09:58:37 +0800

    create new Partners component implement  BRAND_ITEMS from constants

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import PartnersSection from "./components/PartnersSection";
 const App = () => {
   return (
     <main className="text-sm text-secondary antialiased">
       <Navbar />
       <Home />
+      <PartnersSection />
     </main>
   );
 };
