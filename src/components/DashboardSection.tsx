70e72b5a65c27b1a91fd25559b4524eb1b077ad8
alonzojoe <alonzojoenell2k15@gmail.com>
2025-09-03 08:52:09 +0800

    implement DashboardSection img + responsiveness

--- src/components/DashboardSection.tsx
+++ src/components/DashboardSection.tsx
@@ -1,7 +1,15 @@
+import { DASHBOARD_IMG } from "../constants/index";
+
 const DashboardSection = () => {
   return (
     <section className="my-20">
-      <div className="max-w-7xl mx-auto bg-red-500">Dashboard Section</div>
+      <div className="max-w-7xl px-3 py-2 mx-auto">
+        <img
+          className="w-[100%] h-auto"
+          src={DASHBOARD_IMG}
+          alt="dashboard-img"
+        />
+      </div>
     </section>
   );
 };
